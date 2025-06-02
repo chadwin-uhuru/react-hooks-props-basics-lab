9a32eb9e636c82589c982cc2cc5216f70c8338ff
chadwin-uhuru <uhuruchadwin166@gmail.com>
2025-06-02 22:32:33 +0300

    Completed props lab with dynamic components

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,20 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
-
 function App() {
+  const user = {
+    name: "Liza",
+    city: "New York",
+    bio: "I made this!",
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/"
+  };
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} />
+      <About bio={user.bio} github={user.github} linkedin={user.linkedin} />
     </div>
   );
 }
